9275d9977ce32ddf4b2fbfa40389946f5de66439
DrNessquik <boss.69.brian@hotmail.fr>
2022-10-18 08:29:31 +0200

    Commit_1

---
+++ aleatoire_place.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+#define ACHAT_MAX 7
+
+void attente_aleatoire(unsigned int delais) {
+
+/* Initialisation du désordre */
+srandom(time(NULL) % delais);
+
+/* Attente */
+sleep((unsigned int)(random() % ATTENTE_MAX));
+
+}
---
+++ aleatoire_traitement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+#define TEMPS_TRAITEMENT_MAX 3 
+
+void attente_aleatoire(unsigned int delais) {
+
+/* Initialisation du désordre */
+srandom(time(NULL) % delais);
+
+/* Attente */
+sleep((unsigned int)(random() % TEMPS_TRAITEMENT_MAX));
+
+}
